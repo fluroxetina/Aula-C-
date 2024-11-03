457b11ca38c73e8865f227ff332728c869bd4e34
ArthurGuedes <166062249+fluroxetina@users.noreply.github.com>
2024-11-03 13:48:11 -0300

    Update Exercicio1.cs

--- Exemplo.3/Exercicio1.cs
+++ Exemplo.3/Exercicio1.cs
@@ -1,32 +1,32 @@
-﻿// using System;
+﻿using System;
 
-// namespace Exemplo3
-// {
-//     class Program
-//     {
-//         static void Exemplo()
-//         {
-//             System.Console.WriteLine("Escreva sua Idade: ");
-//             int Idade = int.Parse(Console.ReadLine());
+namespace Exemplo3
+{
+    class Program
+    {
+        static void Exemplo()
+        {
+            System.Console.WriteLine("Escreva sua Idade: ");
+            int Idade = int.Parse(Console.ReadLine());
 
-//             switch (Idade)
-//             {
-//                 case >=18 && < 100:
-//                     System.Console.WriteLine("Maior de idade");
-//                     break;
+            switch (Idade)
+            {
+                case >=18 && < 100:
+                    System.Console.WriteLine("Maior de idade");
+                    break;
 
-//                 case < 18:
-//                     System.Console.WriteLine("Menor de idade");
-//                     break;
+                case < 18:
+                    System.Console.WriteLine("Menor de idade");
+                    break;
 
-//                 default:
-//                     System.Console.WriteLine("Morreu");
-//                     break;
+                default:
+                    System.Console.WriteLine("Morreu");
+                    break;
 
-//             }  
-//         }
-//     }
-// }
+            }  
+        }
+    }
+}
 
 
 
