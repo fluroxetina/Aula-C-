76783ac43e3aa1f8085b00ecffc307de2c75a566
ArthurGuedes <166062249+fluroxetina@users.noreply.github.com>
2024-11-03 13:47:04 -0300

    Update Exercicio5.cs

--- Exemplo.3/Exercicio5.cs
+++ Exemplo.3/Exercicio5.cs
@@ -1,48 +1,48 @@
-// using System;
+using System;
 
-// namespace Calculadora
-// {
-//     class FazendoCalculos
-//     {
-//         static void Main(string[] args)
-//         {
-//             // System.Console.WriteLine("Digite o valor da tabuada que deseja ver:");
-//             // int valor = int.Parse(Console.ReadLine());
+namespace Calculadora
+{
+    class FazendoCalculos
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Digite o valor da tabuada que deseja ver:");
+            int valor = int.Parse(Console.ReadLine());
             
-//             // for(int i = 0; i <= 10; i++){
-//             //     System.Console.WriteLine($"{i} x {valor} = {i*valor}");
+            for(int i = 0; i <= 10; i++){
+                System.Console.WriteLine($"{i} x {valor} = {i*valor}");
                 
-//             // }
+            }
 
 
 
 
 
-//             // for(int i = 0; i <= 10; i++){
-//             //     System.Console.WriteLine($"Tabuada do {i}");
-//             //     for(int x = 0; x <= 10; x++){
-//             //         System.Console.WriteLine($"{i} x {x} = {i*x}");
+            for(int i = 0; i <= 10; i++){
+                System.Console.WriteLine($"Tabuada do {i}");
+                for(int x = 0; x <= 10; x++){
+                    System.Console.WriteLine($"{i} x {x} = {i*x}");
 
-//             //     }
+                }
 
-//             // }
+            }
 
-//             int cont = 0;
-//             int cont2 = 0;
+            int cont = 0;
+            int cont2 = 0;
 
-//             while(cont <= 10){
-//                 System.Console.WriteLine($"{cont} x {cont2} = {cont*cont2}");
-//                 if (cont2 == 10){
-//                     cont++;
-//                     cont2 = 0;
-//                 }
-//                 else{
-//                     cont2++;
-//                 }
-//             }
+            while(cont <= 10){
+                System.Console.WriteLine($"{cont} x {cont2} = {cont*cont2}");
+                if (cont2 == 10){
+                    cont++;
+                    cont2 = 0;
+                }
+                else{
+                    cont2++;
+                }
+            }
 
 
 
-//         }
-//     }
-//  }
+        }
+    }
+ }
