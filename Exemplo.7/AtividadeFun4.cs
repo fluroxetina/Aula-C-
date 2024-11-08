ba1de8a2eade12a5f8ff0d0cd808aeef3e54e082
ArthurGuedes <166062249+fluroxetina@users.noreply.github.com>
2024-11-08 14:10:15 -0400

    Update AtividadeFun4.cs

--- Exemplo.7/AtividadeFun4.cs
+++ Exemplo.7/AtividadeFun4.cs
@@ -1,17 +1,11 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
+using System;
 
 namespace Eceme
 {
     public class Mudificadore
     {
-        public int somaDiagonal(int [,] a)
+        public int[] somaDiagonal(int [,] a)
         {
             int somaDiagonal1 = 0;
             int somaDiagonal2 = 0;
@@ -32,7 +26,6 @@ namespace Eceme
                 }
 
             }
-            
             int[] Dia = new int[2] {somaDiagonal1, somaDiagonal2};
             return Dia;
 
@@ -60,26 +53,3 @@ namespace Eceme
 }
 
 
-
-
-//   int[,] mat = new int[3, 3] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-//             int soma = 0;
-//             int soma2 = 0;
-
-//             for (int i = 0; i < mat.GetLength(0); i++)
-//             {
-//                 for (int j = 0; j < mat.GetLength(1); j++)
-//                 {
-//                     if (i == j) 
-//                     {
-//                         soma += mat[i, j];
-//                     }
-//                     if (i + j == mat.GetLength(0) - 1) 
-//                     {
-//                         System.Console.WriteLine(mat[i, j]);
-//                         soma2 += mat[i, j];
-//                     }
-//                 }
-//             }
-
-//             Console.WriteLine($"A soma das diagonais é: {soma} e {soma2}");
