00bdf92bb13f6a3af4730fe826cca912f47e409c
ArthurGuedes <166062249+fluroxetina@users.noreply.github.com>
2024-11-18 19:45:09 -0400

    Update ExercicioRecurção4.cs

--- Exemplo.10/ExercicioRecurção4.cs
+++ Exemplo.10/ExercicioRecurção4.cs
@@ -1,33 +1,33 @@
-// using System;
+using System;
 
-// namespace Exemplo10
-// {
+namespace Exemplo10
+{
 
-//     //5. Faça um programa que calcule a soma dos elementos de um vetor de inteiros.
+    //5. Faça um programa que calcule a soma dos elementos de um vetor de inteiros.
 
-//     class Program
-//     {   
+    class Program
+    {   
 
-//         static int SomaVetor(int[] a, int b, int soma = 0)
-//         {   
+        static int SomaVetor(int[] a, int b, int soma = 0)
+        {   
             
-//             if (b == a.Length){
-//                 return soma;
-//             }
+            if (b == a.Length){
+                return soma;
+            }
 
-//             else{
-//                 
-//                 return SomaVetor(a, b + 1, soma + a[b]);
-//             }
+            else{
+                
+                return SomaVetor(a, b + 1, soma + a[b]);
+            }
 
 
-//         }
-//         static void Main(string[] args)
-//         {
-//             int[ ] Lol = {1,2,3,4,5,6,7,8,9,10}; 
+        }
+        static void Main(string[] args)
+        {
+            int[ ] Lol = {1,2,3,4,5,6,7,8,9,10}; 
 
-//             System.Console.WriteLine(SomaVetor(Lol, 0));
+            System.Console.WriteLine(SomaVetor(Lol, 0));
 
-//         }
-//     }
-// }
+        }
+    }
+}
