ce79a239a21bea43d1d9a76e0b4e4cf6f759d720
ArthurGuedes <166062249+fluroxetina@users.noreply.github.com>
2024-11-18 19:43:02 -0400

    Update atividade1.cs

--- Exemplo.12/atividade1.cs
+++ Exemplo.12/atividade1.cs
@@ -1,58 +1,58 @@
-// using System;
+using System;
 
-// namespace Exemplo12
-// {
-//     public class BubleSort
-//     {
-//         static void BubbleSort(int[] array)
-//         {
-//             int aux;
+namespace Exemplo12
+{
+    public class BubleSort
+    {
+        static void BubbleSort(int[] array)
+        {
+            int aux;
 
-//             for (int i = 0; i < array.Length; i++)
-//             {
-//                 for (int j = 0; j < array.Length - 1; j++)
-//                 {
-//                     if (array[j] > array[j + 1])
-//                     {
-//                         aux = array[j];
-//                         array[j] = array[j + 1];
-//                         array[j + 1] = aux;
-//                     }
-//                 }
-//             }
-//         }
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        aux = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = aux;
+                    }
+                }
+            }
+        }
 
-//         static void Imprimir(int[] array)
-//         {
-//             for (int i = 0; i < array.Length; i++)
-//             {
-//                 Console.Write(array[i] + " ");
-//             }
+        static void Imprimir(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
 
             
-//         }
+        }
 
-//         static void Main(string[] args)
-//         {
-//             int[] ar = new int [10];
+        static void Main(string[] args)
+        {
+            int[] ar = new int [10];
             
-//             for(int i = 0 ; i < 10 ; i++)
-//             {
-//                 Console.WriteLine("Digite um número");
-//                 int num = int.Parse(Console.ReadLine());
+            for(int i = 0 ; i < 10 ; i++)
+            {
+                Console.WriteLine("Digite um número");
+                int num = int.Parse(Console.ReadLine());
                 
-//                 ar[i] = num;
-//             }
+                ar[i] = num;
+            }
 
-//             Console.WriteLine("Array original:");
-//             Imprimir(ar);
+            Console.WriteLine("Array original:");
+            Imprimir(ar);
 
-//             BubbleSort(ar);
+            BubbleSort(ar);
 
-//             Console.WriteLine("\nArray ordenado:");
-//             Imprimir(ar);
+            Console.WriteLine("\nArray ordenado:");
+            Imprimir(ar);
 
             
-//         }
-//     }
-// }
+        }
+    }
+}
